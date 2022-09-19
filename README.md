c43ca9040b5bde773794c23dc524e155d6e1f093
Falcol <bvngoc2000@gmail.com>
2022-09-19 21:14:54 +0700

    config base.html v1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # room_hotel_booking
+[example login](https://github.com/itsvinayak/user_login_and+_register)
+https://www.geeksforgeeks.org/django-sign-up-and-login-with-confirmation-email-python/
--- app/settings.py
+++ app/settings.py
@@ -32,12 +32,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.admin", "django.contrib.auth",
+    "django.contrib.contenttypes", "django.contrib.sessions",
+    "django.contrib.messages", "django.contrib.staticfiles", 'crispy_forms',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -142,7 +140,11 @@ MEDIA_URL = '/media/'
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+CRISPY_TEMPLATE_PACK = 'bootstrap'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'authentication.User'
