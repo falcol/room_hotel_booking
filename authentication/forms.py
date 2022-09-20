85f64780d8f5a110df73892ba4959460f6ede02e
Falcol <bvngoc2000@gmail.com>
2022-09-20 20:59:53 +0700

    login signup v1

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,9 +4,22 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(max_length=20)
+    username = forms.Field(
+        label='Ten tai khoan',
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(
+        label='Email',
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.Field(
+        label='Mat khau',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.Field(
+        label='Nhap lai mat khau',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
--- authentication/views.py
+++ authentication/views.py
@@ -9,6 +9,7 @@ from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth import authenticate, login, logout
 from .tokens import generate_token
 from decouple import config
+from .forms import UserRegisterForm
 
 
 # Create your views here.
@@ -18,75 +19,66 @@ def home(request):
 
 def signup(request):
     if request.method == "POST":
-        username = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
+        form = UserRegisterForm(request.POST)
+        print(form.error_messages)
+        if form.is_valid():
+            form.save()
+            username = request.POST.get('username')
 
-        if User.objects.filter(username=username):
-            messages.error(
+            myuser = User.objects.get(username=username)
+
+            myuser.is_active = False
+            myuser.save()
+            print(myuser)
+            messages.success(
                 request,
-                "Username already exist! Please try some other username.")
-            return redirect('home')
+                "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account."
+            )
+            print(1)
 
-        if User.objects.filter(email=email).exists():
-            messages.error(request, "Email Already Registered!!")
-            return redirect('home')
+            # Welcome Email
+            try:
+                subject = "Welcome to GFG- Django Login!!"
+                message = "Hello " + myuser.username + "!! \n" + "Welcome to GFG!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"
+                from_email = config("EMAIL_HOST_USER")
+                to_list = [myuser.email]
+                send_mail(subject,
+                          message,
+                          from_email,
+                          to_list,
+                          fail_silently=True)
+            except Exception as e:
+                print(e)
+            print(2)
+            # Email Address Confirmation Email
+            try:
+                current_site = get_current_site(request)
+                email_subject = "Confirm your Email @ GFG - Django Login!!"
+                message2 = render_to_string(
+                    'email_confirmation.html', {
+                        'name': myuser.username,
+                        'domain': current_site.domain,
+                        'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+                        'token': generate_token.make_token(myuser)
+                    })
+                email = EmailMessage(
+                    email_subject,
+                    message2,
+                    config("EMAIL_HOST_USER"),
+                    [myuser.email],
+                )
+                email.fail_silently = True
+                email.send()
+            except Exception as e:
+                print(e)
+            print(3)
+            return redirect('signin')
+    else:
+        form = UserRegisterForm()
 
-        if len(username) > 20:
-            messages.error(request, "Username must be under 20 charcters!!")
-            return redirect('home')
+    context = {'register_form': form}
 
-        if pass1 != pass2:
-            messages.error(request, "Passwords didn't matched!!")
-            return redirect('home')
-
-        if not username.isalnum():
-            messages.error(request, "Username must be Alpha-Numeric!!")
-            return redirect('home')
-
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
-        # myuser.is_active = False
-        myuser.is_active = False
-        myuser.save()
-        messages.success(
-            request,
-            "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account."
-        )
-
-        # Welcome Email
-        subject = "Welcome to GFG- Django Login!!"
-        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to GFG!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"
-        from_email = config("EMAIL_HOST_USER")
-        to_list = [myuser.email]
-        send_mail(subject, message, from_email, to_list, fail_silently=True)
-
-        # Email Address Confirmation Email
-        current_site = get_current_site(request)
-        email_subject = "Confirm your Email @ GFG - Django Login!!"
-        message2 = render_to_string(
-            'email_confirmation.html', {
-                'name': myuser.first_name,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-                'token': generate_token.make_token(myuser)
-            })
-        email = EmailMessage(
-            email_subject,
-            message2,
-            config("EMAIL_HOST_USER"),
-            [myuser.email],
-        )
-        email.fail_silently = True
-        email.send()
-
-        return redirect('signin')
-
-    return render(request, "authentication/signup.html")
+    return render(request, "authentication/signup.html", context)
 
 
 def activate(request, uidb64, token):
