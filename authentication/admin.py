6ffd856203a54df6cfb167003e04ce899d0c029d
Falcol <bvngoc2000@gmail.com>
2022-09-26 20:48:49 +0700

    admin auth

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+
 
 # Register your models here.
+@admin.register(User)
+class BaseUser(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser',
+                    'is_active')
+    list_filter = ('username', )
