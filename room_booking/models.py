e2270f4886d64d9917678f923bb7ae21db91e4b2
falcol <bvngoc2000@gmail.com>
2022-11-08 19:41:53 +0700

    fix

--- room_booking/models.py
+++ room_booking/models.py
@@ -12,7 +12,7 @@ class DrinkAndFood(models.Model):
         (1, "Nước uống"),
     )
     hotel_id = models.ForeignKey(HotelDetails, on_delete=models.CASCADE, related_name="hotel_menu")
-    menu_type = models.IntegerField(choices=MENU_TYPE, max_length=1, default=0)
+    menu_type = models.IntegerField(choices=MENU_TYPE, default=0)
     item_name = models.CharField(max_length=255, unique=True)
     total = models.IntegerField(default=0)
     price = models.FloatField(default=0)
